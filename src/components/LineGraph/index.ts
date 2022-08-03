0e6bb1cb70dda6929fd8475a603a2003f897f546
kimmyoungji <kmjoyit21045@gmail.com>
2022-08-03 12:21:14 +0900

    first commit

---
+++ src/components/LineGraph/index.ts
@@ -0,0 +1,1 @@
+export { default as LineGraph } from './index.svelte';
---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script lang="ts"> 
+    
+
+    let title:string = 'line graph'
+    let data:number[] = [10,12,31,8,11,50]
+    let max:number = Math.max(...data);
+    let min:number = Math.min(...data);
+    
+</script>
+
+<div class="root">
+    <div class="graph-field">
+        <LineGraph {title} {data} bind:{max} bind:{min}/>
+    </div>
+</div>
